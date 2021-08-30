7de341d6bdc145498b85bec7cae179033b3bd8ab
sreekar9601 <sreekarsarvepalli9601@gmail.com>
2021-08-30 15:05:12 +0530

    test

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.post('/api/change-password', async (req, res) => {
 		return res.json({ status: 'error', error: 'Invalid password' })
 	}
 
-	if (plainTextPassword.length < 5) {
+	if (plainTextPassword.length < 10) {
 		return res.json({
 			status: 'error',
 			error: 'Password too small. Should be atleast 6 characters'
