614c2ec55c1804e55aa2dc5c4a571b0c6d0466d1
nicolausmaximus <aniket261234@gmail.com>
2021-12-06 03:20:02 +0530

    nodejs sync upload files

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user')
 var http = require('http');
 var path = require('path');
+var multer = require('multer');
+const fs = require('fs');
 var session = require('express-session');
-var port= process.env.PORT || 8200;
+var port = process.env.PORT || 8200;
 global.bcrypt = require('bcrypt');
 var connection = mysql.createConnection({
 	host: 'localhost',
@@ -15,6 +16,35 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 global.db = connection;
+
+
+
+const { google } = require('googleapis');
+const CLIENT_ID = '826335259601-4bmomvi77v3kp3ouknuc7h09af96vvck.apps.googleusercontent.com'
+const CLIENT_SECRET = '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';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = '1//04ImDK9Ro8CYbCgYIARAAGAQSNwF-L9IrQWZGecT00MTP-crnNyWxKDSz02nHLU06FtFwmLVeSc0sHG__os2elJU_6y0xC0a8Wac';
+const oauth2Client = new google.auth.OAuth2(
+	CLIENT_ID,
+	CLIENT_SECRET,
+	REDIRECT_URI
+);
+
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, '/home/aniket/desktop/DBMS/public/uploads/');
+	},
+	filename: function (req, file, cb) {
+		cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+	}
+});
+
+var upload = multer({ storage: storage });
+
+
 var app = express();
 app.use(session({
 	secret: 'secret',
@@ -38,8 +68,24 @@ app.post('/register', routes.register);
 app.get('/dashboard', routes.dashboard);
 app.get('/dashboard/cs', routes.cs);
 app.get('/logout', routes.logout);
-app.get('/profile',routes.profile);
-
-app.listen(port, function(){
-	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.get('/profile', routes.profile);
+app.get('/dashboard/cs/dbms', routes.dbms);
+app.post('/dashboard/cs/dbms', upload.single('dataFile'), routes.dbms);
+app.get("/api/getFiles", async (req, res) => {
+	try {
+	  const files = await File.find();
+	  res.status(200).json({
+	    status: "success",
+	    files,
+	  });
+	} catch (error) {
+	  res.json({
+	    status: "Fail",
+	    error,
+	  });
+	}
       });
+
+app.listen(port, function () {
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,51 @@
-exports.index = async function(req, res){
+exports.index = async function (req, res) {
         var message = '';
-        var sess = req.session; 
-        if(req.method == "POST"){
+        var sess = req.session;
+        if (req.method == "POST") {
                 var email = req.body.email;
-	var username = req.body.username;
-	var password = req.body.password;
-	if (username && password && email) {
-		db.query('SELECT password FROM accounts WHERE username = ? AND email=?', [username, email], async (error, results) => {
-			if (error) throw error;
-			if (results.length > 0) {
-				const reslt = await bcrypt.compare(password, results[0].password);
-				console.log(res);
-				if (reslt) {
-					req.session.loggedin = true;
-					req.session.username = username;
-                                        req.session.email= email;
-                                        req.session.user = results[0];
-                                        console.log(username);
-					res.redirect('/dashboard');
-				}
-				else {
-					message = 'Please enter correct password.';
-                                        res.render('index',{message: message});
-				}
-				res.end();
-			}
-			else {
-				message='Invalid details';
-				res.render('index',{message: message});
-			}
-			res.end();
-		});
-	} else {
-		res.send('Please enter Username and Password!');
-		res.end();
-	}
-          
+                var username = req.body.username;
+                var password = req.body.password;
+                if (username && password && email) {
+                        db.query('SELECT password FROM accounts WHERE username = ? AND email=?', [username, email], async (error, results) => {
+                                if (error) throw error;
+                                if (results.length > 0) {
+                                        const reslt = await bcrypt.compare(password, results[0].password);
+                                        console.log(res);
+                                        if (reslt) {
+                                                req.session.loggedin = true;
+                                                req.session.username = username;
+                                                req.session.email = email;
+                                                req.session.user = results[0];
+                                                console.log(username);
+                                                res.redirect('/dashboard');
+                                        }
+                                        else {
+                                                message = 'Please enter correct password.';
+                                                res.render('index', { message: message });
+                                        }
+                                        res.end();
+                                }
+                                else {
+                                        message = 'Invalid details';
+                                        res.render('index', { message: message });
+                                }
+                                res.end();
+                        });
+                } else {
+                        res.send('Please enter Username and Password!');
+                        res.end();
+                }
+
         } else {
                 message = '';
-                res.render('index',{message: message});
-             }
+                res.render('index', { message: message });
+        }
 
 };
 
-exports.register = async function(req, res){
+exports.register = async function (req, res) {
         message = '';
-        if(req.method == "POST"){
+        if (req.method == "POST") {
                 var emailid = req.body.email;
                 var un = req.body.username;
                 const pwd = await bcrypt.hash(req.body.password, 10);
@@ -54,64 +54,113 @@ exports.register = async function(req, res){
                         db.query("INSERT INTO accounts SET ?", values, function (error, results, fields) {
                                 if (error) {
                                         message = 'Please enter valid details.';
-                                        res.render('registration',{message: message});
+                                        res.render('registration', { message: message });
                                 }
                                 else {
                                         req.session.loggedin = true;
-					req.session.username = un;
-                                        req.session.email= emailid;
+                                        req.session.username = un;
+                                        req.session.email = emailid;
                                         req.session.user = results[0];
                                         console.log(un);
-					res.redirect('/dashboard');
+                                        res.redirect('/dashboard');
                                 }
                         });
                 } else {
                         message = 'Please enter valid details.';
-                        res.render('registration',{message: message});
+                        res.render('registration', { message: message });
                 }
-     
+
         } else {
-                res.render('registration',{message: message});
+                res.render('registration', { message: message });
         }
-     };
-
-
-
-
-
-exports.dashboard = function(req, res){
-	username = req.session.username;
-        email=req.session.email;
-	if(!username && !email){
-		res.redirect("/login");
-		return;
-	}		   
-	res.render('dashboard', {username:username});	  
 };
 
-exports.logout=function(req,res){
-        req.session.destroy(function(err) {
-           res.redirect("/login");
+
+
+
+
+exports.dashboard = function (req, res) {
+        username = req.session.username;
+        email = req.session.email;
+        if (!username && !email) {
+                res.redirect("/login");
+                return;
+        }
+        res.render('dashboard', { username: username });
+};
+
+exports.logout = function (req, res) {
+        req.session.destroy(function (err) {
+                res.redirect("/login");
         })
 };
 
-exports.profile = function(req, res){
+exports.profile = function (req, res) {
         username = req.session.username;
-	email=req.session.email;
-	if(!username && !email){
-		res.redirect("/login");
-		return;
-	}
-     
-           res.render('profile.ejs',{username:username,email:email});
-     };
+        email = req.session.email;
+        if (!username && !email) {
+                res.redirect("/login");
+                return;
+        }
 
-     exports.cs = function(req, res){
-	username = req.session.username;
-        email=req.session.email;
-	if(!username && !email){
-		res.redirect("/login");
-		return;
-	}		   
-	res.render('cs', {username:username,email:email});	  
+        res.render('profile.ejs', { username: username, email: email });
+};
+
+exports.cs = function (req, res) {
+        username = req.session.username;
+        email = req.session.email;
+        if (!username && !email) {
+                res.redirect("/login");
+                return;
+        }
+
+        res.render('cs', { username: username, email: email });
+};
+
+
+exports.dbms = function (req, res) {
+        if (req.method == "POST") {
+                const file = req.file;
+                if (!file) {
+                        message = 'Invalid file';
+                        var sql = 'SELECT * FROM files';
+                        db.query(sql, function (err, data) {
+                                if (err) throw err;
+                                res.render('upload', { message: message, userData: data });
+                        });
+                }
+                else {
+                        var sql = "INSERT INTO files(name) VALUE ('" + req.file.filename + "')";
+                        db.query(sql, function (err, result) {
+                                if (err) {
+                                        message = 'Invalid file';
+                                        res.render('upload', { message });
+                                }
+                                else {
+                                        message='Successful'
+                                        var sql = 'SELECT * FROM files';
+                        db.query(sql, function (err, data, fields) {
+                                if (err) throw err;
+                                res.render('upload', { message: message, userData: data });
+                        });
+                                }
+                        });
+                }
+        }
+        else {
+                username = req.session.username;
+                email = req.session.email;
+                message = '';
+                if (!username && !email) {
+                        res.redirect("/login");
+                        return;
+                }
+                else {
+                        var sql = 'SELECT * FROM files';
+                        db.query(sql, function (err, data, fields) {
+                                if (err) throw err;
+                                res.render('upload', { message: message, userData: data });
+                        });
+                }
+        }
 };
