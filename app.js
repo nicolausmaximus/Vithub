4881f5adaefd7ac9936e6e5b3b636fd35fdd74ad
nicolausmaximus <aniket261234@gmail.com>
2021-12-05 20:06:05 +0530

    improved get and post req readability by adding functions

--- app.js
+++ app.js
@@ -1,102 +1,45 @@
 var mysql = require('mysql');
 var express = require('express');
-
+var routes = require('./routes');
+var user = require('./routes/user')
+var http = require('http');
 var path = require('path');
 var session = require('express-session');
-const bcrypt = require('bcrypt');
+var port= process.env.PORT || 8200;
+global.bcrypt = require('bcrypt');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: '',
 	database: 'nodelogin'
 });
-
+connection.connect();
+global.db = connection;
 var app = express();
 app.use(session({
 	secret: 'secret',
 	resave: false,
-	saveUninitialized: false
+	saveUninitialized: true,
+	cookie: { maxAge: 60000 }
 }));
-app.set('port', process.env.PORT || 8080);
+
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-app.get('/login', function (request, response) {
-	response.sendFile(path.join(__dirname + '/public/login.html'));
-});
 
-app.get('/register', function (request, response) {
-	response.sendFile(path.join(__dirname + '/public/registration.html'));
-});
+app.get('/', routes.index);
+app.get('/login', routes.index);
+app.get('/register', routes.register);
+app.post('/login', routes.index);
+app.post('/register', routes.register);
+app.get('/dashboard', routes.dashboard);
+app.get('/dashboard/cs', routes.cs);
+app.get('/logout', routes.logout);
+app.get('/profile',routes.profile);
 
-app.post('/login', async (request, response) => {
-	var email = request.body.email;
-	var username = request.body.username;
-	var password = request.body.password;
-	if (username && password && email) {
-		connection.query('SELECT password FROM accounts WHERE username = ? AND email=?', [username, email], async (error, results, fields) => {
-			if (error) throw error;
-			if (results.length > 0) {
-				const res = await bcrypt.compare(password, results[0].password);
-				console.log(res);
-				if (res) {
-					request.session.loggedin = true;
-					request.session.username = username;
-					response.redirect('/home');
-					response.end();
-				}
-				else {
-					response.send('Please enter valid Details!');
-					response.end();
-				}
-				response.end();
-			}
-			else {
-				response.send('Incorrect Username and/or Password!');
-				response.end();
-			}
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
-});
-
-app.post('/register', async (request, response) => {
-	var emailid = request.body.email;
-	var un = request.body.username;
-	const pwd = await bcrypt.hash(request.body.password, 10);
-	if (un && pwd && emailid) {
-		var values = { username: un, password: pwd, email: emailid };
-		connection.query("INSERT INTO accounts SET ?", values, function (error, results, fields) {
-			if (error) {
-
-				response.send('Please enter valid Details!');
-				response.end();
-			}
-			else {
-				request.session.loggedin = true;
-				request.session.username = un;
-				response.redirect('/home');
-			}
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
-});
-app.get('/home', function (request, response) {
-	if (request.session.loggedin) {
-		let usrname = request.session.username;
-		response.redirect('/index.html');
-	} else {
-		response.send('Please login to view this page!');
-	}
-	response.end();
-});
-
-app.listen(process.env.PORT || 3000, function(){
+app.listen(port, function(){
 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
       });
---
+++ routes/index.js
@@ -0,0 +1,117 @@
+exports.index = async function(req, res){
+        var message = '';
+        var sess = req.session; 
+        if(req.method == "POST"){
+                var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	if (username && password && email) {
+		db.query('SELECT password FROM accounts WHERE username = ? AND email=?', [username, email], async (error, results) => {
+			if (error) throw error;
+			if (results.length > 0) {
+				const reslt = await bcrypt.compare(password, results[0].password);
+				console.log(res);
+				if (reslt) {
+					req.session.loggedin = true;
+					req.session.username = username;
+                                        req.session.email= email;
+                                        req.session.user = results[0];
+                                        console.log(username);
+					res.redirect('/dashboard');
+				}
+				else {
+					message = 'Please enter correct password.';
+                                        res.render('index',{message: message});
+				}
+				res.end();
+			}
+			else {
+				message='Invalid details';
+				res.render('index',{message: message});
+			}
+			res.end();
+		});
+	} else {
+		res.send('Please enter Username and Password!');
+		res.end();
+	}
+          
+        } else {
+                message = '';
+                res.render('index',{message: message});
+             }
+
+};
+
+exports.register = async function(req, res){
+        message = '';
+        if(req.method == "POST"){
+                var emailid = req.body.email;
+                var un = req.body.username;
+                const pwd = await bcrypt.hash(req.body.password, 10);
+                if (un && pwd && emailid) {
+                        var values = { username: un, password: pwd, email: emailid };
+                        db.query("INSERT INTO accounts SET ?", values, function (error, results, fields) {
+                                if (error) {
+                                        message = 'Please enter valid details.';
+                                        res.render('registration',{message: message});
+                                }
+                                else {
+                                        req.session.loggedin = true;
+					req.session.username = un;
+                                        req.session.email= emailid;
+                                        req.session.user = results[0];
+                                        console.log(un);
+					res.redirect('/dashboard');
+                                }
+                        });
+                } else {
+                        message = 'Please enter valid details.';
+                        res.render('registration',{message: message});
+                }
+     
+        } else {
+                res.render('registration',{message: message});
+        }
+     };
+
+
+
+
+
+exports.dashboard = function(req, res){
+	username = req.session.username;
+        email=req.session.email;
+	if(!username && !email){
+		res.redirect("/login");
+		return;
+	}		   
+	res.render('dashboard', {username:username});	  
+};
+
+exports.logout=function(req,res){
+        req.session.destroy(function(err) {
+           res.redirect("/login");
+        })
+};
+
+exports.profile = function(req, res){
+        username = req.session.username;
+	email=req.session.email;
+	if(!username && !email){
+		res.redirect("/login");
+		return;
+	}
+     
+           res.render('profile.ejs',{username:username,email:email});
+     };
+
+     exports.cs = function(req, res){
+	username = req.session.username;
+        email=req.session.email;
+	if(!username && !email){
+		res.redirect("/login");
+		return;
+	}		   
+	res.render('cs', {username:username,email:email});	  
+};
