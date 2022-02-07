f18c3393a69c0515ee5b38e2ee3a44671fca3dad
nicolausmaximus <aniket261234@gmail.com>
2022-02-08 00:12:38 +0530

    hosto

--- public/js/scripts.js
+++
@@ -1,8 +0,0 @@
-const downloadExcelLink = document.getElementById('downloadExcelLink');
-
-(async() => {
-    const downloadExcelResponse = await fetch('/downloadExcel');
-    const downloadExcelBlob = await downloadExcelResponse.blob();
-    const downloadExcelObjectURL = URL.createObjectURL(downloadExcelBlob);
-    downloadExcelLink.href = downloadExcelObjectURL;
-})();
